0d8deb7b456731ea5ff7ddb7cfb3ac247ac52239
JoaoLucasWolf <159296747+JoaoLucasWolf@users.noreply.github.com>
2024-03-15 19:46:56 +0000

    agendamentoreservas

---
+++ index.js
@@ -0,0 +1,14 @@
+/* function agendamentoDeReservas(sala,numero,data,hora){
+    while (sala) {
+       prompt("Digite o id da sala");
+    }
+}
+    console.log(agendamentoDeReservas()); */
+
+var id = prompt("Digite o id da sala");
+var data = prompt("Digite a data que vc queira reservar");
+var hora = prompt("Digite a hora que deseja reservar");
+
+console.log(id);
+console.log(data);
+console.log(hora);
