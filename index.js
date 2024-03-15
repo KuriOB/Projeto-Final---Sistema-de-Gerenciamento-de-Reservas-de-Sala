113f89e3f27090795bf87a2e04cfdad6683d11c0
R4m0n-4dr1el <159296704+R4m0n-4dr1el@users.noreply.github.com>
2024-03-15 16:25:19 -0300

    Update index.js
    Adicionei a função de procurar o ID, da pra aprimorar e colocar mais coisas a frente.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function gerarId() {
 //Math.random(): Esta função gera um número aleatório entre 500 (inclusivo) e 10000000(exclusivo).
 /*0x10000: Multiplica o número aleatório por 65536 (0x10000 em hexadecimal). Agora, o número aleatório está entre 65536 (inclusivo)
 e 131072 (exclusivo).*/
-    return Math.floor((500 + Math.random()) * 0x10000000).toString(16).substring(1);
+     return Math.floor((500 + Math.random()) * 0x100000000).toString(16).substring(1);
 //Math.floor(...): Arredonda o número para baixo para o número inteiro mais próximo.
 //.toString(16): Converte o número em uma string hexadecimal.
 /*.substring(1): Remove o primeiro caractere da string. Isso é feito porque a conversão
@@ -12,19 +12,38 @@ e 131072 (exclusivo).*/
 }
 //Função gerarIds(quantidade): Esta função gera uma quantidade especificada de IDs únicos e os retorna em um array.
 function gerarIds(quantidade) {
-    var ids = [];
+   var ids = [];
     for (var i = 0; i < quantidade; i++) {
         ids.push(gerarId(10));
     }
     return ids;
 }
+
+function buscarId(id, ids) {
+    return ids.indexOf(id);
+}
+
 /*A função aceita um parâmetro quantidade que especifica quantos IDs devem ser gerados.
 Um array vazio ids é inicializado para armazenar os IDs gerados.
 Um loop for é usado para gerar a quantidade especificada de IDs. Para cada iteração do loop, a função gerarId() 
 é chamada para gerar um ID único, que é então adicionado ao array ids.
 Finalmente, o array ids é retornado.*/
 
-// Exemplo de uso:
-console.log(gerarIds(20)); 
-//gerarIds() escolhe a quantidadede Ids que voce quer gerar.
+var ids = gerarIds(20); 
+console.log(ids);
+
+//Procurar o ID gerado
+function buscarId(id, ids) {
+    return ids.indexOf(id);
+//Pede ao usuário o ID e retorna o indice que ele se encontra no array
+var idParaBuscar = prompt("Digite o ID que deseja buscar:"); // Solicita ao usuário que digite um ID
+var indice = buscarId(idParaBuscar, ids);
+
+if (indice !== -1) {
+    alert('ID encontrado no índice: ' + indice);
+} else {
+    alert('ID não encontrado');
+}
+
+
 
