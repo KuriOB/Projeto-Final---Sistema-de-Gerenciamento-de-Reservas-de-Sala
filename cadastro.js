7ef9361aecc331b90c0ab48cd8dbad882202f7db
Kuri <159294753+KuriOB@users.noreply.github.com>
2024-03-22 15:06:27 -0300

    Update cadastro.js

--- cadastro.js
+++ cadastro.js
@@ -1,31 +1,29 @@
+
 //cadastro de salas
 function cadastro(salaNum, salaDescricao){
-    var salaNum = Number(prompt("Informe o número da sua sala"))
-    var salaDescricao = prompt("informe uma descrição para sua sala")
+    var salaNum = Number(prompt("Informe o número da sua sala"));
+    var salaDescricao = prompt("informe uma descrição para sua sala");
     var guardarSala = [salaNum, salaDescricao];
-    
-    var username = localStorage.getItem("sala");
 
     // Verifica se há um array armazenado no localStorage
-var salaCadastro = localStorage.getItem("cadastros");
+var items = localStorage.getItem("items");
 
 // Se não houver um array armazenado, inicializa-o como um array vazio
-if (!salaCadastro) {
+if (!items) {
     items = [];
 } else {
     // Se houver um array armazenado, converte-o de volta para um array JavaScript
-    items = JSON.parse(salaCadastro);
+    items = JSON.parse(items);
 }
 
 // Adiciona um novo item ao array
 items.push(guardarSala);
 
 // Salva o array atualizado de volta no localStorage
-localStorage.setItem("cadastros", JSON.stringify(salaCadastro));
-console.log(salaCadastro)
-
+localStorage.setItem("items", JSON.stringify(items));
+console.log(items)
 
     console.log(guardarSala)
         return guardarSala
-       }  
-       cadastro()
+       }
+    cadastro()
