c37f0bce127647d6f669772ebc78abc5ed2e3386
JoaoLucasWolf <159296747+JoaoLucasWolf@users.noreply.github.com>
2024-04-10 15:16:11 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var ids = ['01', '02', '03', '04', '05', 
-'06', '07', '08', '09', '10', 
+var ids = ['1', '2', '3', '4', '5', 
+'6', '7', '8', '9', '10', 
 '11', '12', '13', '14', '15',
 '16', '17', '18', '19', '20'];
 
@@ -56,14 +56,6 @@ var data = prompt('Qual a DATA que você deseja reservar ? \n(formato DD/MM/AAAA
 var hora = prompt('Qual a HORA que você deseja reservar ? \n(formato HH:MM)');
 
 agendarReserva(numeroSala, data, hora);
-
-
-
-
-
-
-
-
 
 
 
